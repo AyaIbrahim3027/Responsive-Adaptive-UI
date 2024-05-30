7f78209ff86b3d8c626dc6188b11bb9b5cfd0455
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-05-30 15:43:07 +0200

    add user info list tile

---
+++ lib/views/widgets/custom_drawer.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/views/widgets/user_info_list_tile.dart';
+
+import '../../utils/app_images.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        children: [
+          UserInfoListTile(
+            image: Assets.imagesAvatar3,
+            title: 'Lekan Okeowo',
+            subtitle: 'demo@gmail.com',
+          ),
+        ],
+      ),
+    );
+  }
+}
