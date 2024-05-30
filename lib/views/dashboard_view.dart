534dd83581b3e440dd4c768504964e07ce2fc8ac
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-05-30 15:06:53 +0200

    add assets images

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
-
+import 'package:responsive_dashboard/views/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dashboard/views/widgets/dashboard_desktop_layout.dart';
 
 class DashBoardView extends StatelessWidget {
   const DashBoardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: AdaptiveLayoutWidget(
+        mobileLayout: (context) => SizedBox(),
+        tabletLayout: (context) => SizedBox(),
+        desktopLayout: (context) => DashBoardDesktopLayout(),
+      ),
+    );
   }
 }
