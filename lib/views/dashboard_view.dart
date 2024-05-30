1f67a5ef96c15d6c0f988f44799f7247d9b91007
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-05-30 14:15:08 +0200

    add AdaptiveLayoutWidget

---
+++ lib/views/dashboard_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class DashBoardView extends StatelessWidget {
+  const DashBoardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
