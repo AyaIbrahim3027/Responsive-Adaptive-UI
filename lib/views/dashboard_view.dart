3bd5ddd7a62f29e37b330ea821fe63d9fe521c50
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-06-08 14:47:45 +0200

    Dashboard tablet & mobile layout done

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/views/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dashboard/views/widgets/custom_drawer.dart';
 import 'package:responsive_dashboard/views/widgets/dashboard_desktop_layout.dart';
+import 'package:responsive_dashboard/views/widgets/dashboard_mobile_layout.dart';
+import 'package:responsive_dashboard/views/widgets/dashboard_tablet_layout.dart';
 
-class DashBoardView extends StatelessWidget {
+class DashBoardView extends StatefulWidget {
   const DashBoardView({super.key});
+
+  @override
+  State<DashBoardView> createState() => _DashBoardViewState();
+}
+
+class _DashBoardViewState extends State<DashBoardView> {
+
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
+      appBar: MediaQuery.sizeOf(context).width < 800
+          ? AppBar(
+              leading: IconButton(
+                onPressed: (){
+                  scaffoldKey.currentState!.openDrawer();
+                },
+                icon: const Icon(
+                  Icons.menu,
+                ),
+              ),
+              elevation: 0,
+              backgroundColor: const Color(0xffFAFAFA),
+            )
+          : null,
       backgroundColor: const Color(0xffF7F9FA),
+      drawer:
+          MediaQuery.sizeOf(context).width < 800 ? const CustomDrawer() : null,
       body: AdaptiveLayoutWidget(
-        mobileLayout: (context) => const SizedBox(),
-        tabletLayout: (context) => const SizedBox(),
+        mobileLayout: (context) => const DashBoardMobileLayout(),
+        tabletLayout: (context) => const DashBoardTabletLayout(),
         desktopLayout: (context) => const DashBoardDesktopLayout(),
       ),
     );
