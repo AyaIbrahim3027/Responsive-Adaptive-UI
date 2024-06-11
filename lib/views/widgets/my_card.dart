e4a676d423cc8b48d37cb4a180bcf6b31b34d6ea
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-06-11 14:43:54 +0200

    Responsive desktop layout

--- lib/views/widgets/my_card.dart
+++ lib/views/widgets/my_card.dart
@@ -64,7 +64,7 @@ class MyCard extends StatelessWidget {
                 ],
               ),
             ),
-            const SizedBox(height: 54 - 28,)
+            const Flexible(child: SizedBox(height: 54 - 28,))
           ],
         ),
       ),
